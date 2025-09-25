12a5ab07465038dd1b277b18c936b20ddd84724b
dsar <dsaranda89@ucatolica.edu.co>
2025-09-24 19:13:53 -0500

    fixing dbengine, create the functions to SQLModel

--- DBengine.py
+++ DBengine.py
@@ -1,9 +1,20 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL, echo=True)
-SQLModel.metadata.create_all(engine)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
